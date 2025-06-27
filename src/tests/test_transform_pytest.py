1e8df4b6030a72a399c4cf9c5aeb1ae60b805340
Bram Vermeulen <bram@vermeulenmail.nl>
2025-06-27 09:43:39 +0200

    Updated test_transform_pytest.py to flake8 standards

--- src/tests/test_transform_pytest.py
+++ src/tests/test_transform_pytest.py
@@ -7,7 +7,7 @@ import pytest
                          [(1, np.pi),
                           (0, 0),
                           (2.1, np.pi * 2.1**2)]
-                        ) # noqa: E124
+                        )  # noqa: E124
 def test_area_circ(myinput, myref):
     assert tf.area_circ(myinput) == myref
 
